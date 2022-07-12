f0145db673d317f7d5421fa03171fa2799951131
gabriela.alonso <gabrielaalonsolima@yahoo.com.br>
2022-07-11 21:55:43 -0300

    alteraçao

--- Rh/admin.py
+++ Rh/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from Rh.models import Funcionario, Departamento
 
+
+class FuncioanrioAdmin (admin.ModelAdmin):
+    list_display = ['nome', 'departamento', 'cargo']
+    list_filter = ['departamento', 'cargo']
+
 # Register your models here.
-admin.site.register(Funcionario)
+admin.site.register(Funcionario, FuncioanrioAdmin)
 admin.site.register(Departamento)
--- Rh/models.py
+++ Rh/models.py
@@ -18,6 +18,10 @@ class Funcionario(models.Model):
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     data_nascimento = models.DateField(null=True)
 
+
+class Meta :
+    ordering = ['nome']
+
 class Departamento(models.Model):
     nome = models.CharField(max_length=20)
 
